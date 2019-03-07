c8219c4d4090b095ad46f49d8d56a35eec580b7e
mukul <mukkul007@gmail.com>
2019-03-07 09:39:15 -0500

    NewCom

---
+++ about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+    <title>Setup Instructions</title>
+
+      <?php require("".dirname(__FILE__)."/bootstrap.php") ?>
+</head>
+
+<body>
+
+  <div id="wrapper">
+
+    <div class="col-md-3">
+        <?php require("".dirname(__FILE__)."/sidebar.php") ?>
+</div>
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+
+
+
+
+
+                        <h1>About</h1>
+
+ <p>This application was created mainly due to the lack of practical environments available for hacking web services.  Vulnerable applications such as DVWA and Mutillidae focuses more on generic Web application vulnerabilities such as Cross-site Scripting and SQL Injection.</p>
+
+
+<p><br>Lastly, if you want to learn more about web services, checkout the following</p>
+<ul>
+   <li><a href="https://www.youtube.com/watch?v=oPrrFNEasgE">Greg Patton -The API Assessment Primer (AppSecEU) 2015</a></li>
+   <li><a href="https://www.owasp.org/index.php/REST_Security_Cheat_Sheet">OWASP REST Security Cheat Sheet</a></li>
+   <li><a href="http://www.itsecgames.com/">bWAPP (vulnerable application)</a></li>
+    <li><a href="https://github.com/WebGoat/WebGoat">OWASP Webgoat 7 (vulnerable application)</a></li>
+</ul>
---
+++ about/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Setup Instructions</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/dvws/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/dvws/css/simple-sidebar.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+       <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="/dvws/index.html">
+                        Home
+                    </a>
+                </li>
+                <li><a href="/dvws/about/">About</a></li>
+                <li><a href="/dvws/about/instructions.php">Setup instructions</a></li>
+                <li><a href="/dvws/appinfo.php">PHP Information</a></li>
+        
+                                <li class="sidebar-brand">
+                    <a href="/dvws/vulnerabilities/">
+                        Vulnerabilities
+                    </a>
+                <li><a href="/dvws/vulnerabilities/wsdlenum/">WSDL Enumeration</a></li>
+                <li><a href="/dvws/vulnerabilities/xmlbomb/xmlbomb.php">XML Bomb Denial-of-Service</a></li>
+                <li><a href="/dvws/vulnerabilities/xxe/">XML External Entity Injection</a></li>
+                <li><a href="/dvws/vulnerabilities/xpath/xpath.php">XPATH Injection</a></li>
+                <li><a href="/dvws/vulnerabilities/cmdi/client.php">Command Injection</a></li>
+                <li><a href="/dvws/vulnerabilities/xst/xst.php">Cross Site Tracing (XST)</a></li>
+                <li><a href="/dvws/vulnerabilities/ssrf/">Server Side Request Forgery</a></li>
+                <li><a href="/dvws/vulnerabilities/sqli/">REST API SQL Injection</a></li>
+				   <li><a href="/dvws/vulnerabilities/xxe2/">XML External Entity Injection 2</a></li>
+                
+            </ul>
+            
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+ 
+ 
+ 
+ 
+ 
+                        <h1>About</h1>
+                        
+ <p>This application was created mainly due to the lack of practical environments available for hacking web services.  Vulnerable applications such as DVWA and Mutillidae focuses more on generic Web application vulnerabilities such as Cross-site Scripting and SQL Injection.</p>                             
